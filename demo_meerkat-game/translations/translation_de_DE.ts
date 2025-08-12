e29bf43c69cc29ba1756ceb957c7c4883a0690e8
Hannes Eilers <hannes.eilers@fh-kiel.de>
2025-08-12 10:00:35 +0200

    Fixed quiz spelling bug.
    Signed-off-by: Hannes Eilers <hannes.eilers@fh-kiel.de>

--- demo_meerkat-game/translations/translation_de_DE.ts
+++ demo_meerkat-game/translations/translation_de_DE.ts
@@ -7,7 +7,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hallo liebe Kinder. Ich bin Max. Gut, dass ihr da seid. Ich brauche dringend eure Hilfe. Bei uns im Zoo ist ein Erdmänchen ausgebüchst. Nun versteckt es sich in einem anderen Gehege. Helft ihr mir beim Finden des Erdmänchens?</source>
             <comment>Text</comment>
-            <translation type="unfinished">Hallo liebe Kinder. Ich bin Max. Gut, dass ihr da seid. Ich brauche dringend eure Hilfe. Bei uns im Zoo ist ein Erdmänchen ausgebüchst. Nun versteckt es sich in einem anderen Gehege. Helft ihr mir beim Finden des Erdmänchens?</translation>
+            <translation>Hallo liebe Kinder. Ich bin Max. Gut, dass ihr da seid. Ich brauche dringend eure Hilfe. Bei uns im Zoo ist ein Erdmänchen ausgebüxt. Nun versteckt es sich in einem anderen Gehäge. Helft ihr mir beim Finden des Erdmänchens?</translation>
         </message>
     </context>
     <context>
@@ -16,7 +16,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Sehr schön, aber wir müssen aufpassen, dass wir nicht auffallen. Somitversucht so gut wie möglich die anderen Tiere, bei denen wir nachschauen, nachzumachen. Bekommt ihr das hin?</source>
             <comment>Text</comment>
-            <translation type="unfinished">Sehr schön, aber wir müssen aufpassen, dass wir nicht auffallen. Somitversucht so gut wie möglich die anderen Tiere, bei denen wir nachschauen, nachzumachen. Bekommt ihr das hin?</translation>
+            <translation>Sehr schön, aber wir müssen aufpassen, dass wir nicht auffallen. Versucht so gut wie möglich die anderen Tiere, bei denen wir nachschauen, nachzumachen. Bekommt ihr das hin?</translation>
         </message>
     </context>
     <context>
@@ -25,7 +25,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Dann kann es losgehen</source>
             <comment>Text</comment>
-            <translation type="unfinished">Dann kann es losgehen</translation>
+            <translation>Dann kann es losgehen!</translation>
         </message>
     </context>
     <context>
@@ -34,7 +34,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Vielleicht bei den Elefanten? Begrüßt doch die Elefanten mit viel Töröö.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Vielleicht bei den Elefanten? Begrüßt doch die Elefanten mit viel Töröö.</translation>
+            <translation>Vielleicht bei den Elefanten? Begrüßt doch die Elefanten mit viel Töröh.</translation>
         </message>
     </context>
     <context>
@@ -43,7 +43,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Ja wir haben es gefunden. Nun lasst es uns unauffällig zurücksetzen in das richtige Gehege. Ohne die anderen Erdmänchen zu erschrecken.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Ja wir haben es gefunden. Nun lasst es uns unauffällig zurücksetzen in das richtige Gehege. Ohne die anderen Erdmänchen zu erschrecken.</translation>
+            <translation>Ja wir haben es gefunden. Nun lasst es uns unauffällig zurücksetzen in das richtige Gehege. Ohne die anderen Erdmänchen zu erschrecken.</translation>
         </message>
     </context>
     <context>
@@ -52,7 +52,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Vielen dank das ihr mir geholfen habt. Toll habt ihr das gemacht</source>
             <comment>Text</comment>
-            <translation type="unfinished">Vielen dank das ihr mir geholfen habt. Toll habt ihr das gemacht</translation>
+            <translation>Vielen dank das ihr mir geholfen habt. Toll habt ihr das gemacht!</translation>
         </message>
     </context>
     <context>
@@ -61,7 +61,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Lasst uns im Giraffengehege nachschauen. Vielleicht ist es da.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Lasst uns im Giraffengehege nachschauen. Vielleicht ist es da.</translation>
+            <translation>Lasst uns im Giraffengehege nachschauen. Vielleicht ist es da.</translation>
         </message>
     </context>
     <context>
@@ -70,7 +70,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Ich sehe das Kängurugehege. Lasst uns dort nachschauen. Hüpft, wie Kängurus. hoffentlich haben wir Glück</source>
             <comment>Text</comment>
-            <translation type="unfinished">Ich sehe das Kängurugehege. Lasst uns dort nachschauen. Hüpft, wie Kängurus. hoffentlich haben wir Glück</translation>
+            <translation>Ich sehe das Kängurugehege. Lasst uns dort nachschauen. Hüpft, wie Kängurus. hoffentlich haben wir Glück</translation>
         </message>
     </context>
     <context>
@@ -79,7 +79,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Eine Möglichkeit wäre das Mäusegehege. Nun müssen wir ganz leise sein. Sonst entdeckt es uns.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Eine Möglichkeit wäre das Mäusegehege. Nun müssen wir ganz leise sein. Sonst entdeckt es uns.</translation>
+            <translation>Eine Möglichkeit wäre das Mäusegehege. Nun müssen wir ganz leise sein. Sonst entdeckt es uns.</translation>
         </message>
     </context>
     <context>
@@ -88,7 +88,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hier ist das Pferdegehege. Vielleicht ist hier das Erdmännchen? Schauen wir nach, ob es sich hier versteckt.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Hier ist das Pferdegehege. Vielleicht ist hier das Erdmännchen? Schauen wir nach, ob es sich hier versteckt.</translation>
+            <translation>Hier ist das Pferdegehege. Vielleicht ist hier das Erdmännchen? Schauen wir nach, ob es sich hier versteckt.</translation>
         </message>
     </context>
     <context>
@@ -97,7 +97,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Das Schlangenterrarium! Vielleicht hat es sich verkrochen. Schlängelt euch wie Schlangen über den Boden. Vielleicht entdeckt ihr es?</source>
             <comment>Text</comment>
-            <translation type="unfinished">Das Schlangenterrarium! Vielleicht hat es sich verkrochen. Schlängelt euch wie Schlangen über den Boden. Vielleicht entdeckt ihr es?</translation>
+            <translation>Das Schlangenterrarium! Vielleicht hat es sich verkrochen. Schlängelt euch wie Schlangen über den Boden. Vielleicht entdeckt ihr es?</translation>
         </message>
     </context>
     <context>
@@ -106,7 +106,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Vielleicht bei den Elefanten? Begrüßt doch die Elefanten mit viel Töröö.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Vielleicht bei den Elefanten? Begrüßt doch die Elefanten mit viel Töröö.</translation>
+            <translation>Vielleicht bei den Elefanten? Begrüßt doch die Elefanten mit viel Töröh.</translation>
         </message>
     </context>
     <context>
@@ -115,7 +115,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>das Erdmännchen hat sich aber gut versteckt. lasst uns nicht aufgeben.</source>
             <comment>Text</comment>
-            <translation type="unfinished">das Erdmännchen hat sich aber gut versteckt. lasst uns nicht aufgeben.</translation>
+            <translation>Das Erdmännchen hat sich aber gut versteckt. lasst uns nicht aufgeben.</translation>
         </message>
     </context>
     <context>
@@ -124,7 +124,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Lasst uns im Giraffengehege nachschauen. Vielleicht ist es da.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Lasst uns im Giraffengehege nachschauen. Vielleicht ist es da.</translation>
+            <translation>Lasst uns im Giraffengehege nachschauen. Vielleicht ist es da.</translation>
         </message>
     </context>
     <context>
@@ -133,7 +133,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hm, anscheinend war hier das Erdmännchen nicht. Lasst uns zum nächsten Gehege gehen.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Hm, anscheinend war hier das Erdmännchen nicht. Lasst uns zum nächsten Gehege gehen.</translation>
+            <translation>Anscheinend war hier das Erdmännchen nicht. Lasst uns zum nächsten Gehege gehen.</translation>
         </message>
     </context>
     <context>
@@ -142,7 +142,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Ich sehe das Kängurugehege. Lasst uns dort nachschauen. Hüpft, wie Kängurus. hoffentlich haben wir Glück</source>
             <comment>Text</comment>
-            <translation type="unfinished">Ich sehe das Kängurugehege. Lasst uns dort nachschauen. Hüpft, wie Kängurus. hoffentlich haben wir Glück</translation>
+            <translation>Ich sehe das Kängurugehege. Lasst uns dort nachschauen. Hüpft, wie Kängurus. Hoffentlich haben wir Glück.</translation>
         </message>
     </context>
     <context>
@@ -151,7 +151,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Oh man, das war nichts wo kann es nur sein?</source>
             <comment>Text</comment>
-            <translation type="unfinished">Oh man, das war nichts wo kann es nur sein?</translation>
+            <translation>Oh man, das war nichts wo kann es nur sein?</translation>
         </message>
     </context>
     <context>
@@ -160,7 +160,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Eine Möglichkeit wäre das Mäusegehege. Nun müssen wir ganz leise sein. Sonst entdeckt es uns.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Eine Möglichkeit wäre das Mäusegehege. Nun müssen wir ganz leise sein. Sonst entdeckt es uns.</translation>
+            <translation>Eine Möglichkeit wäre das Mäusegehege. Nun müssen wir ganz leise sein. Sonst entdeckt es uns.</translation>
         </message>
     </context>
     <context>
@@ -169,7 +169,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hier ist es nicht zu finden, weitergehts.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Hier ist es nicht zu finden, weitergehts.</translation>
+            <translation>Hier ist es nicht zu finden, weiter gehts.</translation>
         </message>
     </context>
     <context>
@@ -178,7 +178,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hier ist das Pferdegehege. Vielleicht ist hier das Erdmännchen? Schauen wir nach, ob es sich hier versteckt.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Hier ist das Pferdegehege. Vielleicht ist hier das Erdmännchen? Schauen wir nach, ob es sich hier versteckt.</translation>
+            <translation>Hier ist das Pferdegehege. Vielleicht ist hier das Erdmännchen? Schauen wir nach, ob es sich hier versteckt.</translation>
         </message>
     </context>
     <context>
@@ -187,7 +187,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Mist, da war es nicht.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Mist, da war es nicht.</translation>
+            <translation>Mist, da war es nicht.</translation>
         </message>
     </context>
     <context>
@@ -196,7 +196,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Das Schlangenterrarium! Vielleicht hat es sich verkrochen. Schlängelt euch wie Schlangen über den Boden. Vielleicht entdeckt ihr es?</source>
             <comment>Text</comment>
-            <translation type="unfinished">Das Schlangenterrarium! Vielleicht hat es sich verkrochen. Schlängelt euch wie Schlangen über den Boden. Vielleicht entdeckt ihr es?</translation>
+            <translation>Das Schlangenterrarium! Vielleicht hat es sich verkrochen. Schlängelt euch wie Schlangen über den Boden. Vielleicht entdeckt ihr es?</translation>
         </message>
     </context>
     <context>
@@ -205,7 +205,7 @@
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Schade, hier ist es nicht.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Schade, hier ist es nicht.</translation>
+            <translation>Schade, hier ist es nicht.</translation>
         </message>
     </context>
 </TS>
